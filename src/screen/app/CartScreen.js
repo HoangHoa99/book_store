698706fc1b13952d2409eb4772a1f1b013c347d0
HoangHoa99 <hoangthaihoa2122@gmail.com>
2021-05-02 00:42:05 +0700

    add modal to cart screen

--- src/screen/app/CartScreen.js
+++ src/screen/app/CartScreen.js
@@ -1,7 +1,19 @@
 import { Feather } from "@expo/vector-icons";
 import React, { useContext, useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet, View, Text, Image, Alert } from "react-native";
-import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Alert,
+  Modal,
+} from "react-native";
+import {
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+} from "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import colors from "../../assets/color/colors";
 import { PrimaryButton } from "../../component/Button";
@@ -9,6 +21,8 @@ import { AppContext } from "../HomeScreen";
 
 export default function CartScreen({ navigation }) {
   const cartContext = useContext(AppContext);
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   const [key, setKey] = useState(0);
 
@@ -25,8 +39,10 @@ export default function CartScreen({ navigation }) {
     return unsubscribe;
   }, [navigation, key]);
 
-  async function clearCart() {
-    cartContext.setCartItems([]);
+  function clearCart() {
+    // setModalVisible(false);
+    // Alert.alert("Checkout", "Submit your order");
+    // cartContext.setCartItems([]);
   }
 
   /** SECTION - Handle cart item */
@@ -136,6 +152,94 @@ export default function CartScreen({ navigation }) {
   };
   return (
     <SafeAreaView style={{ backgroundColor: colors.white, flex: 1 }}>
+      <Modal
+        animationType="slide"
+        presentationStyle="pageSheet"
+        visible={modalVisible}
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 100,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "#FFF",
+            padding: 20,
+          }}
+        >
+          <Text style={{ fontSize: 25, marginTop: 20 }}>
+            Thông tin khách hàng{" "}
+          </Text>
+          {/* <Text style={{ fontSize: 16, color: "gray", marginTop: 20 }}>
+        Sign up to continue
+      </Text> */}
+
+          <TextInput
+            style={{
+              marginTop: 40,
+              borderBottomColor: "#ddd",
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+            placeholder="Số điện thoại"
+            onChangeText={(text) => setEmailOrPhone(text)}
+          />
+
+          <TextInput
+            style={{
+              marginTop: 40,
+              borderBottomColor: "#ddd",
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+            placeholder="Họ & Tên"
+            onChangeText={(text) => setUsername(text)}
+          />
+
+          <TextInput
+            style={{
+              marginTop: 40,
+              borderBottomColor: "#ddd",
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+            placeholder="Địa chỉ"
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+          />
+
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 40,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => setModalVisible(!modalVisible)}
+              style={{
+                width: 200,
+                backgroundColor: "#0d47a1",
+                padding: 10,
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 40,
+                marginTop: 30,
+              }}
+            >
+              <Text
+                style={{ textAlign: "center", color: "#FFF", fontSize: 16 }}
+              >
+                Signup
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
       <View style={style.header}>
         <Icon name="arrow-back-ios" size={28} onPress={navigation.goBack} />
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>Cart</Text>
@@ -163,7 +267,10 @@ export default function CartScreen({ navigation }) {
           </Text>
         </View>
         <View style={{ marginHorizontal: 30 }}>
-          <PrimaryButton title="CHECKOUT" onPress={() => clearCart()} />
+          <PrimaryButton
+            title="CHECKOUT"
+            onPress={() => setModalVisible(!modalVisible)}
+          />
         </View>
       </View>
     </SafeAreaView>
@@ -196,5 +303,46 @@ const style = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
