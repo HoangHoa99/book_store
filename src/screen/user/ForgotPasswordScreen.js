9e88d66334743c8f4434d4135735186b7132c0f5
HoangHoa99 <hoangthaihoa2122@gmail.com>
2021-07-22 00:18:22 +0700

    update forgot password

--- src/screen/user/ForgotPasswordScreen.js
+++ src/screen/user/ForgotPasswordScreen.js
@@ -1,20 +1,25 @@
-import React, { useState, useContext } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
-import {ForgotPasswordAsync} from '../../service/UserService';
+import { ForgotPasswordAsync } from "../../service/UserService";
 import Loading from "../Loading";
-import { AppContext } from "../HomeScreen";
 
 export default function ForgotPasswordScreen({ navigation }) {
-  const appContext = useContext(AppContext);
-
   const [email, setEmail] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function sendEmail() {
-    appContext.setLoading(true);
+    setLoading(true);
     await ForgotPasswordAsync(email).then((res) => {
-      appContext.setLoading(false);
+      setLoading(false);
       if (res.Type === "success") {
         Alert.alert(
           "Check your email!",
@@ -34,61 +39,68 @@ export default function ForgotPasswordScreen({ navigation }) {
   }
   return (
     <>
-    {appContext.loading ? (
-      <>
-      <Loading />
-      </>
-    ) : (
-      <>
-      <View style={styles.container}>
-      <Text style={{ fontSize: 25, marginTop: 80 }}>Forgot your password?</Text>
-      <Text style={{ fontSize: 16, color: "gray", marginTop: 20 }}>
-        Your email 
-      </Text>
+      {loading ? (
+        <>
+          <Loading />
+        </>
+      ) : (
+        <>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+              <Text style={{ fontSize: 25, marginTop: 80 }}>
+                Forgot your password?
+              </Text>
+              <Text style={{ fontSize: 16, color: "gray", marginTop: 20 }}>
+                Your email
+              </Text>
 
-        <TextInput
-          style={{
-            marginTop: 40,
-            borderBottomColor: "#ddd",
-            borderBottomWidth: 1,
-            paddingBottom: 20,
-          }}
-          placeholder="Email"
-          onChangeText={(text) => setEmail(text)}
-        />
+              <TextInput
+                style={{
+                  marginTop: 40,
+                  borderBottomColor: "#ddd",
+                  borderBottomWidth: 1,
+                  paddingBottom: 20,
+                }}
+                placeholder="Email"
+                onChangeText={(text) => setEmail(text)}
+              />
 
-        <Text style={{ color: "red", textAlign: "center", marginTop: 10 }}>
-          {errorMsg}
-        </Text>
-        
+              <Text
+                style={{ color: "red", textAlign: "center", marginTop: 10 }}
+              >
+                {errorMsg}
+              </Text>
 
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: 40,
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => sendEmail()}
-          style={{
-            width: 200,
-            backgroundColor: "#0d47a1",
-            padding: 10,
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: 40,
-            marginTop: 30,
-          }}
-        >
-          <Text style={{ textAlign: "center", color: "#FFF", fontSize: 16 }}>
-            Send
-          </Text>
-        </TouchableOpacity>        
-      </View>
-    </View>
-      </>
-    )}
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: 40,
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => sendEmail()}
+                  style={{
+                    width: 200,
+                    backgroundColor: "#0d47a1",
+                    padding: 10,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: 40,
+                    marginTop: 30,
+                  }}
+                >
+                  <Text
+                    style={{ textAlign: "center", color: "#FFF", fontSize: 16 }}
+                  >
+                    Send
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </>
+      )}
     </>
   );
 }
